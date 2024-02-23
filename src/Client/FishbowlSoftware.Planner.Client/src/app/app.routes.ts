02c5ada3935054e7111884f91b6092afbf972cfb
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-02-23 02:24:05 -0500

    added prettier to the angular project

--- src/Client/FishbowlSoftware.Planner.Client/src/app/app.routes.ts
+++ src/Client/FishbowlSoftware.Planner.Client/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import {type Routes} from '@angular/router';
+import {Routes} from '@angular/router';
+import {Error404Component} from './pages/error404';
+import {UnauthorizedComponent} from './pages/unauthorized';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home').then(m => m.HOME_ROUTES),
+  },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedComponent,
+  },
+  {
+    path: '404',
+    component: Error404Component,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+];
